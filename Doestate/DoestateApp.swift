28b090c4762ffebb47f4b2f0a3a9c94b7d3a2934
kiddo4 <olanrewajutaiwo183@gmail.com>
2025-09-27 01:12:48 +0100

    Initial Commit

---
+++ Doestate/DoestateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoestateApp.swift
+//  Doestate
+//
+//  Created by Olanrewaju Taiwo Smith on 27/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct DoestateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DoestateTests/DoestateTests.swift
@@ -0,0 +1,16 @@
+//
+//  DoestateTests.swift
+//  DoestateTests
+//
+//  Created by Olanrewaju Taiwo Smith on 27/09/2025.
+//
+
+import Testing
+
+struct DoestateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
